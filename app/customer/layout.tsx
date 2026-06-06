ac5b0c9d0cf6fb9375f4417b24c2d38ed676da96
samanehsoltani935 <samanehsoltani935@gmail.com>
2026-06-06 20:42:23 +0330

    Fix Vercel build issues

--- app/customer/layout.tsx
+++ app/customer/layout.tsx
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
+export const dynamicParams = true;
 
 import PanelLayout from "@/components/panel/PanelLayout";
 
 export default PanelLayout;
-
